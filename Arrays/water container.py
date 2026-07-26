7886ca5ccdd18c587576c2c5d8502229f08ac3cc
satya-mishra <302017286+satyamishra06@users.noreply.github.com>
2026-07-26 17:54:22 +0530

    container with most water problem

---
+++ Arrays/water container.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+
+        n = len(height)
+        max_area = 0
+        left = 0
+        right = n-1
+        for i in range(n):
+            while left<right:
+                width = right-left
+                h = min(height[left] , height[right])
+                area = width*h
+                max_area = max(max_area,area)
+                if height[left]<height[right]:
+                    left+=1
+                else:
+                    right-=1
+        return max_area                
+
+                
+
+
+
+
+
+
+        
+
