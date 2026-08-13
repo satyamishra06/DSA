528127a74512cc8eeaebbf49fe5fbfb91ae75ba4
satya-mishra <302017286+satyamishra06@users.noreply.github.com>
2026-08-14 00:42:05 +0530

    square of a sorted array

---
+++ Arrays/square f sorted array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left = 0
+        right = n-1
+        ans = [0]*n
+        for i in range(n-1,-1,-1):
+            if abs(nums[left])>abs(nums[right]):
+                ans[i]= nums[left]**2
+                left+=1
+            else:
+                ans[i]=nums[right]**2
+                right-=1
+        return ans        
+
+            
+            
+
+
+
+        
